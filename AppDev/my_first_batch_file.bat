c1f1cebb585617506fc65246c1cfbdb9e7387bb7
Cherry Mae Elgario <74056148+CherryMaeElgario0420@users.noreply.github.com>
2021-03-30 20:52:46 +0800

    git add.
    "first commit"

---
+++ AppDev/my_first_batch_file.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+ECHO Congratulations! Your first batch file executed successfully.
+PAUSE
+```
+
+- @ECHO OFF - disables the display prompt to show only the message on a clean line. 
+-  ECHO - prints the desired text on the screen
+- PAUSE - Prevents the console window from closing after executing the command. 
